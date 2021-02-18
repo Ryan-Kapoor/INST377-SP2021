fc6ffdcfb0a8e4599f8528d7ca61de41a11011d2
Ryan-Kapoor <78117561+Ryan-Kapoor@users.noreply.github.com>
2021-02-18 15:00:32 -0500

    Update Lab 3
    Commit lab 3 submission, background is now colored and JavaScript is implemented

--- public/labs/lab_3/script.js
+++ public/labs/lab_3/script.js
@@ -1,1 +1,34 @@
 /* Put your javascript in here */
+
+const imageArray = [
+    'images/onigiri_1.png',
+    'images/onigiri_2.png',
+    'images/onigiri_3.png',
+    'images/onigiri_4.png',
+    'images/roll_1.png',
+    'images/roll_2.png',
+    'images/roll_3.png',
+]
+
+array1.forEach((element) => {
+    console.log(element);
+
+let width = 130; // image width
+let count = 3; // visible images count
+
+let list = carousel.querySelector('ul');
+let listElems = carousel.querySelectorAll('li');
+
+let position = 0;
+
+carousel.querySelector('.prev').onclick = function() {
+    position += width * count;
+    position = Math.min(position, 0)
+    list.style.marginLeft = position + 'px';
+};
+
+carousel.querySelector('.next').onclick = function() {
+    position -= width * count;
+    position = Math.max(position, -width * (listElems.length - count));
+    list.style.marginLeft = position + 'px';
+    };        
